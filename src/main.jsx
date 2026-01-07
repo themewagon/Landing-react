01b7b09010dd5c6940d5ab7b7fe7f32415052039
AnikaBristy <bristy.technext@gmail.com>
2026-01-08 02:02:29 +0600

    gh-page added

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { ThemeContextProvider } from './context/ThemeContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-    <BrowserRouter>
+    <BrowserRouter basename={"/Landing-react"}>
         <ThemeContextProvider>
             <App />
         </ThemeContextProvider>
